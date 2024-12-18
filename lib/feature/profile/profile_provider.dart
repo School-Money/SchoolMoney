21cb155d98e4d81d8fa1d81832af1cbff1832f76
olekgolus02 <olekgolus02@gmail.com>
2024-12-18 23:03:19 +0000

    fix conflict

--- lib/feature/profile/profile_provider.dart
+++ lib/feature/profile/profile_provider.dart
@@ -8,19 +8,16 @@ import 'dart:typed_data';
 class ProfileProvider extends ChangeNotifier {
   final ProfileService _profileService = ProfileService();
   Profile? _profile;
-  Uint8List? _avatar;
   bool _isLoading = false;
 
   Profile? get profile => _profile;
   bool get isLoading => _isLoading;
-  Uint8List? get avatar => _avatar;
 
   Future<void> fetchProfile() async {
     _isLoading = true;
     notifyListeners();
     try {
       _profile = await _profileService.getProfileInfo();
-      _avatar = await _profileService.getProfileImage();
     } catch (e) {
       // nothing
     } finally {
@@ -35,7 +32,7 @@ class ProfileProvider extends ChangeNotifier {
       await _profileService.uploadProfilePhoto(imageInput);
 
       // Refresh the avatar after upload
-      _avatar = await _profileService.getProfileImage();
+      await fetchProfile();
       notifyListeners();
     } catch (e) {
       print('Failed to upload profile photo: $e');
